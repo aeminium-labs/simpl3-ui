16eed778b2a9d5ad763333ab8f843cbfd9a19ede
0xDistro <hey@0xDistro.com>
2024-04-08 02:47:45 +0100

    Remove env variable

--- src/lib/gql.ts
+++ src/lib/gql.ts
@@ -1,8 +1,6 @@
 import { Client, fetchExchange } from "@urql/core";
 
 export const client = new Client({
-    url:
-        process.env.NEXT_PUBLIC_SIMPL3_ENDPOINT ||
-        "https://api.simpl3.dev/graphql",
+    url: "https://api.simpl3.dev/graphql",
     exchanges: [fetchExchange],
 });
