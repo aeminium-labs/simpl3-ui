257093cfbe2f29427f49312b38ac976e2985ef6c
0xDistro <hey@0xDistro.com>
2024-04-08 02:32:16 +0100

    Remove cache from urql

--- src/lib/gql.ts
+++ src/lib/gql.ts
@@ -1,8 +1,8 @@
-import { Client, cacheExchange, fetchExchange } from "@urql/core";
+import { Client, fetchExchange } from "@urql/core";
 
 export const client = new Client({
     url:
         process.env.NEXT_PUBLIC_SIMPL3_ENDPOINT ||
         "https://api.simpl3.dev/graphql",
-    exchanges: [cacheExchange, fetchExchange],
+    exchanges: [fetchExchange],
 });
